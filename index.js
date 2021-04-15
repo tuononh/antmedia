b99d71ec71f42b0da5d7f2d8b0a58dca98258d9d
Phạm Tuấn Anh <tuananh.pham@cimb.com>
2021-04-15 19:13:58 +0700

    Init

---
+++ index.js
@@ -0,0 +1,9 @@
+import { PeerStats } from './peer_stats';
+import { WebSocketAdaptor } from "./websocket_adaptor";
+import { WebRTCAdaptor } from "./webrtc_adaptor";
+
+module.exports = {
+    PeerStats: PeerStats,
+    WebSocketAdaptor: WebSocketAdaptor,
+    WebRTCAdaptor: WebRTCAdaptor
+}
